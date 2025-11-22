e7e083ab31fa59fcde4f2be8c7be3eb30c70dfa9
Auromera <Autoboyrunner@gmail.com>
2025-11-22 11:15:34 +0000

    upd

--- Linkedlist.h
+++ Linkedlist.h
@@ -10,39 +10,34 @@ private:
     Node* head;
 
 public:
-    // Constructor
-    LinkedList() {
-        head = nullptr;
-    }
+    LinkedList() { head = nullptr; }
 
-    // Destructor
     ~LinkedList() {
         Node* curr = head;
         while (curr != nullptr) {
             Node* temp = curr;
-            curr = curr->next;
+            curr = curr->getNext();
             delete temp;
         }
     }
 
-    // Insert at front
     void insertFront(string name, string start, string end, float dist, float co2) {
         Node* newNode = new Node(name, start, end, dist, co2);
-        newNode->next = head;
+        newNode->setNext(head);
         head = newNode;
     }
 
-    // Display all trips
     void display() {
         Node* curr = head;
         while (curr != nullptr) {
-            cout << curr->studentName << " | "
-                 << curr->startStation << " → "
-                 << curr->endStation << " | "
-                 << curr->distance << " km | "
-                 << curr->CO2saved << " kg CO2"
+            cout << curr->getStudentName() << " | "
+                 << curr->getStartStation() << " → "
+                 << curr->getEndStation() << " | "
+                 << curr->getDistance() << " km | "
+                 << curr->getCO2saved() << " kg CO2"
                  << endl;
-            curr = curr->next;
+
+            curr = curr->getNext();
         }
     }
 };
